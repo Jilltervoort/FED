924d00322ed0e4aa549518c6fda00dc389894c28
Jilltervoort <Jill.tervoort@hva.nl>
2025-11-27 11:14:26 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,15 @@
-// JavaScript Document
-console.log("hi");
+const carousel = document.querySelector(".starterCarousel ul");
+const naarRechts = document.querySelector(".button_knoppen button:last-of-type");
+const naarLinks = document.querySelector(".button_knoppen button:first-of-type");
+
+let index = 0;
+
+naarRechts.addEventListener("click", () => {
+    index = Math.min(index + 1, carousel.children.length - 1);
+    carousel.style.transform ='translateX(-' + (index * 100) + 'vw)';
+});
+
+naarLinks.addEventListener("click", () => {
+    index = Math.max(index - 1, 0);
+    carousel.style.transform = 'translateX(-' + (index * 100) + 'vw)';
+});
